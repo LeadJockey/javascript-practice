3ce82fa1af37c3be089d861cb49d0315ede52edb
shawn.thecool <shawn.thecool@kakaocorp.com>
2018-04-02 15:09:53 +0900

    init

---
+++ src/loshawn/Array/difference/index.js
@@ -0,0 +1,21 @@
+const _findDiffer = function (list,target) {
+	const result = [];
+	for(let item of list){
+		if(target !== item){
+			result.push(item);
+		}
+	}
+	return result;
+};
+
+
+const difference = function (rootList, targetList) {
+	let result = rootList.map((e)=>e);
+	
+	for(let target of targetList){
+		result = _findDiffer(result, target);
+	}
+	return result;
+};
+
+module.exports = difference;
