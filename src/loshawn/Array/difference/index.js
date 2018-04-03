bd6229b199a4a3fbde56ad7125719dfa00fbfe47
shawn.thecool <shawn.thecool@kakaocorp.com>
2018-04-03 15:04:52 +0900

    [fix] difference

--- src/loshawn/Array/difference/index.js
+++ src/loshawn/Array/difference/index.js
@@ -1,3 +1,6 @@
+const isArray = require('./../../util/isArray/index');
+const message = require('./../../test/message');
+
 const _findDiffer = function (list,target) {
 	const result = [];
 	for(let item of list){
@@ -10,7 +13,16 @@ const _findDiffer = function (list,target) {
 
 
 const difference = function (rootList, targetList) {
-	let result = rootList.map((e)=>e);
+	
+	if(!isArray(rootList)){
+		throw new TypeError(`compact 의 첫번째 인자는 ${message.mustTypeArray}`);
+	}
+	
+	if(!isArray(targetList)){
+		throw new TypeError(`compact 의 두번째 인자는 ${message.mustTypeArray}`);
+	}
+	
+	let result = rootList.slice(0);
 	
 	for(let target of targetList){
 		result = _findDiffer(result, target);
