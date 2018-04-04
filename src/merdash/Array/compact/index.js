d86620b1ae9b9408fea66be28d194c8ec4e17e77
merlin.ho <merlin.ho@kakakocorp.com>
2018-04-04 18:57:03 +0900

    concat ing

--- src/merdash/Array/compact/index.js
+++ src/merdash/Array/compact/index.js
@@ -1,5 +1,33 @@
+// falsy value : false, null , 0 , "" , undefined , NaN...
+const isArray = require("../../isArray")
+
+function isFalsy(value) {
+  if (!!value) {
+    return false;
+  }
+  return true;
+}
+
 function compact(collection) {
   var result = [];
+
+  if (!isArray(collection) && collection !== undefined) {
+    throw new TypeError("인자는 배열이 들어와야 합니다.");
+  }
+
+  if (collection === undefined) {
+    return result;
+  }
+
+  var tempCollection = collection.slice();
+
+  while (tempCollection.length > 0) {
+    var value = tempCollection.shift();
+
+    if (!isFalsy(value)) {
+      result.push(value);
+    }
+  }
 
   return result;
 }
