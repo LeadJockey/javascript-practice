6cf69b73f2c8ccc47a5320b6fa74379bf3403959
merlin.ho <merlin.ho@kakakocorp.com>
2018-04-05 15:21:59 +0900

    concat

--- src/lodash/merdash/Array/concat/index.js
+++ src/lodash/merdash/Array/concat/index.js
@@ -1,10 +1,23 @@
+const isArray = require("../../isArray")
 
 function concat() {
   var result = [];
   var arrArgs = Array.prototype.slice.call(arguments)  // copy
 
 
+  while (arrArgs.length > 0) {
+    var value = arrArgs.shift();
 
+    if (isArray(value)) {
+      while (value.length > 0) {
+        result.push(value.shift());
+      }
+    } else {
+      result.push(value);
+    }
+
+
+  }
 
 
   return result;
--- src/lodash/merdash/Array/concat/index.test.js
+++ src/lodash/merdash/Array/concat/index.test.js
@@ -12,10 +12,34 @@ describe("concat 함수는", () => {
     ];
 
     args.forEach(element => {
-      expect(concat.apply(null, args.given)).toEqual(args.expect);
+      expect(concat.apply(null, element.given)).toEqual(element.expect);
     });
   })
 
+  test("첫번째 인자만 배열이 들어오면 그 배열 그대로 내뱉는다.", () => {
+    const args = [
+      {
+        given: [[1, 2, 3, 4]],
+        expect: [1, 2, 3, 4]
+      }
+    ];
 
+    args.forEach(element => {
+      expect(concat.apply(null, element.given)).toEqual(element.expect);
+    });
+  })
+
+  test("모든 인자에 대해서 concat을 진행한다.", () => {
+    const args = [
+      {
+        given: [[1, 2, 3, 4], ['a', 'b']],
+        expect: [1, 2, 3, 4, 'a', 'b']
+      }
+    ];
+
+    args.forEach(element => {
+      expect(concat.apply(null, element.given)).toEqual(element.expect);
+    });
+  })
 
 })
