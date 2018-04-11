18120afbc65baf0cf484ac315aaa9a542a9a06f0
LeadJockey <dltldnjs5790@naver.com>
2018-04-11 15:58:55 +0900

    [update] lodash. Lang. isArrayLike 적용

--- src/lodash/loshawn/Global/index.js
+++
@@ -1,5 +0,0 @@
-const global = {
-  MAX_SAFE_INTEGER:9007199254740991
-};
-
-module.exports = global;
--- src/lodash/loshawn/Lang/isArrayLike.test.js
+++ src/lodash/loshawn/Lang/isArrayLike.test.js
@@ -2,13 +2,13 @@ const isArrayLike = require('./isArrayLike');
 
 describe('loshawn isArrayLike method test', () =>{
 
-  test(`given function(){} => expected true`, () =>{
+  test(`given [1, 2, 3] => expected true`, () =>{
     expect(isArrayLike([1, 2, 3])).toBe(true);
   });
-  test(`given /abc/ => expected false`, () =>{
+  test(`given document.body.children => expected true`, () =>{
     expect(isArrayLike(document.body.children)).toBe(true);
   });
-  test(`given /abc/ => expected false`, () =>{
+  test(`given 'abc' => expected true`, () =>{
     expect(isArrayLike('abc')).toBe(true);
   });
 
