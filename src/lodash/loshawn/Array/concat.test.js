507e7c592b95c47697674742489b2b10cd071caa
LeadJockey <dltldnjs5790@naver.com>
2018-04-23 18:57:52 +0900

    [update] lodash loshawn difference fixed

--- src/lodash/loshawn/Array/concat.test.js
+++ src/lodash/loshawn/Array/concat.test.js
@@ -1,1 +1,16 @@
-s
+const concat = require('./concat');
+const _ = require('lodash');
+
+describe('loshawn concat method test', () =>{
+
+  test(`given concat([1,2,3],[4,5],[6,7]) => expected ${_.concat([1,2,3],[4,5],[6,7])}`, () =>{
+    expect(concat([1,2,3],[4,5],[6,7]))
+    .toEqual(_.concat([1,2,3],[4,5],[6,7]));
+  });
+
+  test(`given concat([1,2,3],[4,5],[6,7]) => expected [1,2,3,4,5,6,7]`, () =>{
+    expect(concat([1,2,3],[4,5],[6,7]))
+    .toEqual([1,2,3,4,5,6,7]);
+  });
+
+});
--- src/lodash/loshawn/Array/difference.js
+++ src/lodash/loshawn/Array/difference.js
@@ -15,7 +15,7 @@ const _getOnlyArrayList = function(list){
 };
 
 /**
- * 일종의 차집합을 구하는 함수
+ * 차집합을 구하는 함수
  * 작동하기 위해서는 최소한 두개의 배열 형태의 인자가 필요하다.
  * 첫번째 인자를 베이스라고 지칭한다
  * 두번째 인자부터는 컴페어 라고 자칭한다.
@@ -26,41 +26,43 @@ const _getOnlyArrayList = function(list){
  */
 const difference = function(){
 
-  const result = [];
   const copyList = Array.prototype.slice.call(arguments);
 
   if(copyList.length < 2){
     throw new TypeError(`입력되어야 할 파라미터가 부족합니다.`);
   }
 
-  const baseList = copyList.slice(0, 1);
+  const baseList = copyList.slice(0, 1)[0];
+  // console.log(baseList);
   const compareList = copyList.slice(1);
 
   if(!isArray(baseList)){
-    throw new TypeError(` 파라미터의 형태는 배열이어야 합니dddd다.`);
+    throw new TypeError(` 파라미터의 형태는 배열이어야 합니다.`);
   }
 
   const onlyArrCompareList = _getOnlyArrayList(compareList);
+  // console.log(onlyArrCompareList);
 
-  const mergeCompareList = [];
+  const oacLeng= onlyArrCompareList.length;
   let t = 0;
-  while(t < onlyArrCompareList.length){
-    concat(mergeCompareList,onlyArrCompareList[t]);
+  let mergeCompareList = [];
+
+  while(t < oacLeng){
+    mergeCompareList = concat(mergeCompareList,onlyArrCompareList[t++]);
+    // console.log(`mergetCompareList : ${mergeCompareList}`);
   }
 
-  let i = 0;
-  while(i++ < baseList.length){
+  for(let i=0; i < baseList.length; i++){
 
-    let j = 0;
-    while(j++ < mergeCompareList.length){
+    for(let j = 0; j < mergeCompareList.length; j++){
       if(baseList[i] === mergeCompareList[j]){
-        result.push(baseList[i])
+        console.log(`deleted : ${baseList[i]}`);
+        baseList.splice(i,1);
       }
     }
   }
 
-
-  return baseList;
+  return baseList
 };
 
 module.exports = difference;
