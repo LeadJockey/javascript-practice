2f295b84d096b5531ac12e10d26188f0ea3a770f
LeadJockey <dltldnjs5790@naver.com>
2018-05-08 12:06:23 +0900

    [fixed] components - sequentialSelectBox 완성 ( 주석추가 )

--- src/components/sequentialSelectBox/sequentialSelectBox.js
+++ src/components/sequentialSelectBox/sequentialSelectBox.js
@@ -1,4 +1,4 @@
-
+// 변수 선언부
 const $ctx = $('#wrap');
 const $menu = $ctx.find('.list_menu');
 const $menuList = $menu.find('.menu_item');
@@ -7,12 +7,12 @@ const state = {
   currentMenuIndex:0,
   menus:[
     {
-      isClickAble:true,
-      isContOpened:false,
-      isSelected:false,
-      isChangedText:false,
-      text:'지역시',
-      defaultText:'지역시'
+      isClickAble:true, // 테그 변환 기준 : true => <a></a>, false => <span></span>
+      isContOpened:false, // 메뉴 콘텐츠 노출 기준
+      isSelected:false, // 메뉴 선택 변화 감지 기준
+      isChangedText:false, // 메뉴 text 변화 감지 기준
+      text:'지역시', // 변화 텍스트
+      defaultText:'지역시' // 기본 텍스트
     },
     {
       isClickAble:false,
@@ -33,17 +33,34 @@ const state = {
   ]
 };
 
+// 함수 실행부
 bindEvents();
 render(
   _stateLog,
   menuTab,
-  menuCont,
+  menuCont
 );
+$(state).trigger('state-updated');
 
+// 함수 선언부
+/**
+ * 스테이트의 상태값을 변경할 때는 반드시 이 함수를 사용할 것.
+ * 사용은 기본 스테이트 객체에 새로운 스테이트 객체를 추가하는 식으로 활용.
+ * 기본객체가 변하면 'state-update'라는 커스텀 이벤트를 발생시킴.
+ * @param state
+ * @param newState
+ */
 function setState(state, newState){
   $.extend(state, newState);
   $(state).trigger('state-updated');
 }
+
+/**
+ * 인자받은 콜백 함수들을 실행시켜주는 함수.
+ * 스테이트가 업데이트 될때마다 작동.
+ * 콜백의 인자는 없어야 함.
+ * @param arguments : functions
+ */
 function render(){
   const args = Array.prototype.slice.call(arguments);
 
@@ -55,6 +72,10 @@ function render(){
 
   $(state).on('state-updated', _reDraw);
 }
+
+/**
+ * 이벤트 바인딩 함수
+ */
 function bindEvents(){
   // 메뉴 탬 클릭시
   $menuList.on('click', 'a' ,function(){
@@ -107,6 +128,11 @@ function bindEvents(){
   });
 }
 
+/**
+ * js render 사용시 탬플릿을 캐싱해 주기 위한 함수.
+ * @returns {{getTemplate : getTemplate}}
+ * @private
+ */
 function _templateFactory(){
   const templateContainer = {};
 
@@ -121,10 +147,18 @@ function _templateFactory(){
     getTemplate:getTemplate
   }
 }
+
+/**
+ * 스테이트 변화를 감시하고 콘솔테이블로 'state-update' 발동 시마다 스테이트를 보여줌
+ * @private
+ */
 function _stateLog(){
   console.table(state.menus);
 }
 
+/**
+ * 메뉴탭 관련 랜더링 함수
+ */
 function menuTab(){
   state.menus.forEach(function(menuItem, i){
     const text = menuItem.text || '';
@@ -135,6 +169,9 @@ function menuTab(){
   });
 }
 
+/**
+ * 메뉴 콘텐츠 관련 랜더링 함수
+ */
 function menuCont(){
   //TODO ajex로 데이터 받는 것을 가정 -start
   const DATA1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
@@ -155,4 +192,3 @@ function menuCont(){
   }
 }
 
-$(state).trigger('state-updated');
