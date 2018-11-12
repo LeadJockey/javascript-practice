7ec8e29b4d964d4a50c37ffe5153d350e2dd170f
LeadJockey <dltldnjs5790@naver.com>
2018-11-12 16:36:37 +0900

    [update] frame v1, v2, v3

--- src/patterns/factoryMethod/index.js
+++ src/patterns/factoryMethod/index.js
@@ -3,36 +3,39 @@ const Factory = function(){
 		let employee;
 		// 추상화 시킨 펙토리얼 메서드 맵퍼
 		const employeeTypeMap = {
-			'fulltime':new FullTime(),
-			'parttime':new PartTime(),
-			'temporary':new Temporary(),
-			'contractor':new Contractor()
+			'fulltime':FullTime,
+			'parttime':PartTime,
+			'temporary':Temporary,
+			'contractor':Contractor
 		};
-		
-		employee = employeeTypeMap[type] || function(){};
-		
+
+		employee = new employeeTypeMap[type]() || function(){};
+
 		employee.type = type;
-		
+
 		employee.say = function(){
 			console.log(this.type + ": rate" + this.hourly + "/hour");
 		};
-		
+
 		return employee;
-	}
+	};
+	this.add = function(){};
+	this.remove = function(){};
 };
 
 const FullTime = function(){
+	console.log('f');
 	this.hourly = "$12";
 };
-
 const PartTime = function(){
+	console.log('p');
 	this.hourly = "$11";
 };
-
 const Temporary = function(){
+	console.log('t');
 	this.hourly = "$10";
 };
-
 const Contractor = function(){
+	console.log('c');
 	this.hourly = "$15";
 };
